ceaa32ffe65479ceb7fce19a5a763240591cef3e
David <davidstankoviic@gmail.com>
2021-05-06 19:01:45 +0200

    Implement: Global Styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 
-import './App.css';
 import Home from './pages/home';
 
 function App() {
