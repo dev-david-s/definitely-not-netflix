6e9411bc7ce6b1358d3446f09bc18a632ff65246
David <davidstankoviic@gmail.com>
2021-05-06 22:15:56 +0200

    Add: header component

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,10 +2,20 @@ import React from 'react';
 import { FooterContainer } from '../containers/footer';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
+import { HeaderContainer } from '../containers/header';
+import { OptForm } from '../components';
 
 export default function Home() {
     return (
         <>
+            <HeaderContainer>
+                <OptForm>
+                    <OptForm.Input placeholder="Email Address" />
+                    <OptForm.Button>Get Started</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+                </OptForm>
+            </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
