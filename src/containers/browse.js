70432254e7429a0af5ab3ba3694e242ebf664af7
David <davidstankoviic@gmail.com>
2021-05-09 18:51:24 +0200

    Add: Row, Card and Feature elements on Browse page

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Header } from '../components';
+import { Card, Header } from '../components';
 import { SelectProfileContainer } from './profiles';
 import { FooterContainer } from './footer';
+import Loading from '../components/loading';
 
 import * as ROUTES from '../constants/routes';
 
 import { FirebaseContext } from '../context/firebase';
-import Loading from '../components/loading';
 
-export function BrowseContainer() {
+export function BrowseContainer({ slides }) {
 
     const [profile, setProfile] = useState({});
     const [category, setCategory] = useState('series');
     const [loading, setLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState('');
+    const [slideRows, setSlideRows] = useState([]);
 
     const { firebase } = useContext(FirebaseContext);
 
@@ -27,6 +28,10 @@ export function BrowseContainer() {
             setLoading(false)
         }, 3000)
     }, [user])
+
+    useEffect(() => {
+        setSlideRows(slides[category]);
+    }, [slides, category])
 
     return profile.displayName ? (
         <>
@@ -72,6 +77,29 @@ export function BrowseContainer() {
                     <Header.PlayButton>Play</Header.PlayButton>
                 </Header.Feature>
             </Header>
+
+            <Card.Group>
+                {slideRows.map((slideItem) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map((item) => (
+                                <Card.Item key={item.docId} item={item}>
+                                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                        <Card.Feature category={category}>
+                            <p>I am the feature!</p>
+                        </Card.Feature>
+                    </Card>
+                ))}
+            </Card.Group>
+
             <FooterContainer />
         </>
     ) : (<SelectProfileContainer user={user} setProfile={setProfile} />)
