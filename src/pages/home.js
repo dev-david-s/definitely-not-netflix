70c66aa69ee9bb60ea1df36010e96cd0175ffc4b
David <davidstankoviic@gmail.com>
2021-05-09 13:50:57 +0200

    Add: feature component

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,18 +3,22 @@ import { FooterContainer } from '../containers/footer';
 import { JumbotronContainer } from '../containers/jumbotron';
 import { FaqsContainer } from '../containers/faqs';
 import { HeaderContainer } from '../containers/header';
-import { OptForm } from '../components';
+import { Feature, OptForm } from '../components';
 
 export default function Home() {
     return (
         <>
             <HeaderContainer>
-                <OptForm>
-                    <OptForm.Input placeholder="Email Address" />
-                    <OptForm.Button>Get Started</OptForm.Button>
-                    <OptForm.Break />
-                    <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
-                </OptForm>
+                <Feature>
+                    <Feature.Title>Unlimited films, TV programmes and more.</Feature.Title>
+                    <Feature.SubTitle>Watch anywhere. Cancel anytime.</Feature.SubTitle>
+                    <OptForm>
+                        <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+                        <OptForm.Break />
+                        <OptForm.Input placeholder="Email Address" />
+                        <OptForm.Button>Get Started</OptForm.Button>
+                    </OptForm>
+                </Feature>
             </HeaderContainer>
             <JumbotronContainer />
             <FaqsContainer />
