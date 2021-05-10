2ac07f6674492ae2ca39e3539120223044cb3ba6
dev-david-s <83727822+dev-david-s@users.noreply.github.com>
2021-05-10 22:42:59 +0200

    Remove: Firebase config

--- src/index.js
+++ src/index.js
@@ -9,12 +9,7 @@ import reportWebVitals from './reportWebVitals';
 
 // import { seedDatabase } from './seed';
 const config = {
-  apiKey: "AIzaSyBbwx87qE3kaPw8imQNCAM49PyYz_wbnuA",
-  authDomain: "netflix-clone-acc57.firebaseapp.com",
-  projectId: "netflix-clone-acc57",
-  storageBucket: "netflix-clone-acc57.appspot.com",
-  messagingSenderId: "369968478541",
-  appId: "1:369968478541:web:50da102ff09912a51cdf04"
+  // add your config here
 }
 
 const firebase = window.firebase.initializeApp(config);
